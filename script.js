595dfe2d89373005e22e92e7040626b57b360575
Otilia Visan <otilia.visan22@gmail.com>
2021-03-23 23:18:22 +0000

    added changes to html

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello");
+//start the quiz
+//start the timer 
+https://www.w3schools.com/js/js_timing.asp
+https://www.w3schools.com/howto/howto_js_countdown.asp
+//Display the first question
+//Start button function - Question Title, Option 1, 2, 3, 4 - Clicked correct answer - Next question - Nope, Incorrect, error message.
