40dba4ca817d1f4370cd61524f0ca5da998cbab7
Otilia Visan <otilia.visan22@gmail.com>
2021-03-24 14:33:49 +0000

    added changes to html

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-//start the quiz
-//start the timer 
-https://www.w3schools.com/js/js_timing.asp
-https://www.w3schools.com/howto/howto_js_countdown.asp
-//Display the first question
-//Start button function - Question Title, Option 1, 2, 3, 4 - Clicked correct answer - Next question - Nope, Incorrect, error message.
+const timerSpanElement = document.getElementById("timer");
+const StartQuizButtonElement = document.getElementById("start-btn");
