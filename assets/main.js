2794a1e6a581c84436e58756db4e354ff1920344
Otilia Visan <otilia.visan22@gmail.com>
2021-03-26 12:31:51 +0000

    add questions

--- assets/main.js
+++ assets/main.js
@@ -3,14 +3,62 @@ const quizContainer = document.getElementById("quiz-container");
 const timerSpanElement = document.getElementById("timer");
 
 let timerValue = 5;
+let questionIndex = 0;
+
+const questions = [
+  {
+    title: "Who invented JavaScript?",
+    choices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich"],
+    correctAnswer: "Brendan Eich",
+  },
+  {
+    title: "What are JavaScript Data Types?",
+    choices: [
+      "number,",
+      "string",
+      "boolen",
+      "obejct",
+      "undefined",
+      "all the above",
+    ],
+    correctAnswer: "all the above",
+  },
+  {
+    title: " What are all the types of Pop up boxes available in JavaScript?",
+    choices: ["alert", "confirm", "prompt", "all the above"],
+    correctAnswer: "all the above",
+  },
+  {
+    title: "What is a closure in JavaScript?",
+    choices: [
+      "an inner function that has access to the variables in the outer (enclosing) function’s scope chain",
+      "element",
+      "mind-set",
+    ],
+    correctAnswer:
+      "an inner function that has access to the variables in the outer (enclosing) function’s scope chain",
+  },
+  {
+    title: "Define event bubbling?",
+    choices: [
+      "event-bubble",
+      "data type",
+      "JavaScript allows DOM elements to be nested inside each other",
+    ],
+    correctAnswer:
+      "JavaScript allows DOM elements to be nested inside each other",
+  },
+];
 
 const renderQuestion = () => {
+  const question = questions[questionIndex];
+
   const div = document.createElement("div");
-  div.setAttribute("data-answer", "TODO");
+  div.setAttribute("data-answer", question.correctAnswer);
   div.setAttribute("id", "question");
 
   const h2 = document.createElement("h2");
-  h2.textContent = "TODO";
+  h2.textContent = question.title;
 
   div.appendChild(h2);
 
--- assets/script.js
+++ assets/script.js
@@ -2,88 +2,16 @@ const timerSpanElement = document.getElementById("timer");
 const StartQuizButtonElement = document.getElementById("start-btn");
 const startButton = document.getElementById("start-btn");
 
-
 let timerValue = 60;
 
-const question = [
-  {
-    title: "Who invented JavaScript?",
-    choices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich"],
-    correctAnswer: "Brendan Eich",
-  },
-
-  {
-    title: "What are JavaScript Data Types?",
-    choices: ["number,", "string", "boolen", "obejct", "undefined", "all the above"],
-    correctAnswer: "all the above",
-  },
-
-  {
-    title: " What are all the types of Pop up boxes available in JavaScript?",
-    choices: ["alert", "confirm", "prompt", "all the above"],
-    correctAnswer: "all the above",
-  
-  },
-
-  {
-    title: "What is a "closure" in JavaScript?",
-    choices: ["an inner function that has access to the variables in the outer (enclosing) function’s scope chain", "element", "mind-set"],
-    correctAnswer: "an inner function that has access to the variables in the outer (enclosing) function’s scope chain",
-  },
-
-  {
-    title: "Define event bubbling?",
-    choices: ["event-bubble", "data type", "JavaScript allows DOM elements to be nested inside each other"],
-    correctAnswer: "JavaScript allows DOM elements to be nested inside each other",
-  },
-];
-
-//Add the timer
-const startTimer = () => {
-  const timerTick = () => {
-    timerSpanElement.textContent = timerValue;
-    timerValue -= 1;
-
-    if (timerValue < 0) {
-      clearInterval(timer);
-    }
-  };
-
-  const timer = setInterval(timerTick, 1000);
-};
-
-const createChoices =("choices") =>{ 
-
-  const createChoices = (choice) =>{
-    const QuestionContainer= document.createElement ("questionContainer");
-    const button = document.createElement ("button");
+const createChoices = ("choices") => {
+  const createChoices = (choice) => {
+    const QuestionContainer = document.createElement("questionContainer");
+    const button = document.createElement("button");
     button.setAttribute("data-answer", choices[0]);
-
-  }
+  };
   choices.forEach(createChoices);
 };
-
-
-const createQuestionContainer = (question) =>{
-const div = document.createElement ("div");
-div.setAttribute("data-answer", "TODO");
-div.setAttribute("id", "question");
-
-const h2 = document.createElement ("h2")
-h2.textContent = "TODO"
-
-div.appendChild(h2)
-
-}
-
-
-const startQuiz = () =>{
- //create question container
- createQuestion(Questions[0]);
- //remove the start button container
- //append question container to the DOM
-}
-
 
 StartQuizButtonElement.addEventListener("click", startTimer);
 startButton.addEventListener("click", startButton);
