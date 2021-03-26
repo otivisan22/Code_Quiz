f8fc5e6b073fd6a2f04c6771a52bada82eb664ad
Otilia Visan <otilia.visan22@gmail.com>
2021-03-26 00:33:34 +0000

    added more questions to the quiz

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 const timerSpanElement = document.getElementById("timer");
 const StartQuizButtonElement = document.getElementById("start-btn");
+const startButton = document.getElementById("start-btn");
 
 let timerValue = 60;
 
@@ -11,9 +12,28 @@ const question = [
   },
 
   {
-    title: "Who invented JavaScript?",
-    choices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich"],
-    correctAnswer: "Brendan Eich",
+    title: "3.What are JavaScript Data Types?",
+    choices: ["number,", "string", "boolen", "obejct", "undefined", "all the above"],
+    correctAnswer: "all the above",
+  },
+
+  {
+    title: " What are all the types of Pop up boxes available in JavaScript?",
+    choices: ["alert", "confirm", "prompt", "all the above"],
+    correctAnswer: "all the above",
+  
+  },
+
+  {
+    title: "What is a "closure" in JavaScript?",
+    choices: ["an inner function that has access to the variables in the outer (enclosing) function’s scope chain", "element", "mind-set"],
+    correctAnswer: "an inner function that has access to the variables in the outer (enclosing) function’s scope chain",
+  },
+
+  {
+    title: "Define event bubbling?",
+    choices: ["event-bubble", "data type", "JavaScript allows DOM elements to be nested inside each other"],
+    correctAnswer: "JavaScript allows DOM elements to be nested inside each other",
   },
 ];
 
@@ -31,3 +51,4 @@ const startTimer = () => {
 };
 
 StartQuizButtonElement.addEventListener("click", startTimer);
+startButton.addEventListener("click", startButton);
